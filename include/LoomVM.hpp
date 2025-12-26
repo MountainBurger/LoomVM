3bc8961d598a5d25c741b43870429d433e14a7fb
Robin <robin.sachdeva@gmail.com>
2025-12-26 13:56:38 +0000

    refactor(LoomVM): return boolean instead of void
    - Can use booleans to give information regarding the success of the
      operation (true for success, false otherwise)

--- include/LoomVM.hpp
+++ include/LoomVM.hpp
@@ -3,12 +3,13 @@
 #include <vector>
 
 class LoomVM {
-    public:
-        void loadProgram(const std::vector<int32_t>& program);
-        void run();
-    private:
-        std::vector<int32_t> stack_;
-        std::vector<int32_t> program_;
-        int pc_ = 0;
-        bool isRunning_ = false;
+  public:
+    bool loadProgram(const std::vector<int32_t> &program);
+    bool run();
+
+  private:
+    std::vector<int32_t> stack_;
+    std::vector<int32_t> program_;
+    int pc_ = 0;
+    bool isRunning_ = false;
 };
