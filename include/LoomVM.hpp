b099d2467ec146a7e8ebf8e3c1f5a63c3f9a691f
Robin <robin.sachdeva@gmail.com>
2025-12-26 14:26:49 +0000

    feat(LoomVM): add instruction set to header
    - Operations for halting, pushing to the stack, popping from it,
    adding the top two values, subtracting them, multiplying them, and
    dividing them
    - In subtraction/division, the top value on the stack is the subtrahend/divisor.

--- include/LoomVM.hpp
+++ include/LoomVM.hpp
@@ -7,6 +7,15 @@ class LoomVM {
     bool loadProgram(const std::vector<int32_t> &program);
     bool run();
     void dumpStack() const;
+    enum class Op : int32_t {
+        HLT = 0,  // Halt the machine
+        PSH = 1,  // Push a value to the top of the stack
+        POP = 2,  // Pop the top value from the stack
+        ADD = 3,  // Add the top two values on the stack
+        SUB = 4,  // Subtract the top two values (top is subtrahend)
+        MUL = 5,  // Multiply the top two values
+        DIV = 6,  // Divide the top two values (top is divisor)
+    };
 
   private:
     std::vector<int32_t> stack_;
