fe15fbf78409ce6028c66423bec150470d458bf4
Robin <robin.sachdeva@gmail.com>
2025-12-26 13:57:42 +0000

    feat(LoomVM): implement loadProgram

--- src/LoomVM.cpp
+++ src/LoomVM.cpp
@@ -1,1 +1,15 @@
 #include "LoomVM.hpp"
+#include <vector>
+
+bool LoomVM::loadProgram(const std::vector<int32_t> &program) {
+    // Do not load if currently running
+    if (isRunning_)
+        return false;
+
+    // Load new program
+    program_ = program;
+    // Reset program counter
+    pc_ = 0;
+
+    return true;
+}
